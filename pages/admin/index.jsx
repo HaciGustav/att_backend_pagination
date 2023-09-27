c6f2fc5fb7da949a8f2f13bddf6c071a8ea87aae
Mehmet-FK <mk@atina.at>
2023-09-27 15:44:26 +0200

    change input order

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,12 +1,20 @@
-import { Button, TextField } from "@mui/material";
+import { Button, Paper, TextField } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { tableStyles } from "@/styles/table_styles";
 
 const Admin = () => {
   const [sqlQuery, setSqlQuery] = useState(
-    // "SELECT * FROM ATINA_MobileBookings"
-    ""
+    "SELECT * FROM ATINA_MobileBookings Where UserID = 5656"
   );
+  const [data, setData] = useState([]);
+  const [headers, setHeaders] = useState([]);
 
   const handleSubmit = async () => {
     try {
@@ -22,7 +30,8 @@ const Admin = () => {
         }
       );
 
-      console.log(res);
+      console.log(res.data);
+      setData(res.data);
     } catch (error) {
       console.log(error);
     }
@@ -32,25 +41,82 @@ const Admin = () => {
     setSqlQuery(e.target.value);
   };
 
+  useEffect(() => {
+    if (data.length) {
+      const x = Object.keys(data[0]);
+      setHeaders(x);
+    }
+  }, [data]);
+
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "grid",
-        placeItems: "center",
-        height: "60vh",
-      }}
-    >
-      <TextField
-        sx={{ width: "50%" }}
-        onChange={handleChange}
-        value={sqlQuery || ""}
-        label="Multiline"
-        multiline
-        rows={15}
-      />
-      <Button onClick={() => handleSubmit()}>Submit</Button>
-    </div>
+    <>
+      <div
+        style={{
+          width: "50vw",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "end",
+          rowGap: "8px",
+          padding: "1rem",
+          backgroundColor: "yellow",
+          position: "relative",
+        }}
+      >
+        <TextField
+          sx={{ width: "100%" }}
+          onChange={handleChange}
+          value={sqlQuery || ""}
+          label="Multiline"
+          multiline
+          rows={10}
+          inputProps={{ spellCheck: "false", style: { color: "#ffffff00" } }}
+        ></TextField>
+        <Button variant="contained" onClick={() => handleSubmit()}>
+          Submit
+        </Button>
+      </div>
+
+      <TableContainer
+        component={Paper}
+        sx={{ ...tableStyles.tableContainer, p: 0 }}
+      >
+        <Table sx={{ minWidth: 650, position: "relative" }}>
+          <TableHead sx={{ ...tableStyles.tableHead, top: -2 }}>
+            <TableRow>
+              {headers?.map((header, i) => (
+                <TableCell
+                  key={i}
+                  sx={{
+                    ...tableStyles.th.cell,
+                    minWidth: 100,
+                    p: 1,
+                  }}
+                  align="center"
+                >
+                  {header}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data?.map((row) => (
+              <TableRow key={row.ID}>
+                {headers?.map((cell, i) => (
+                  <TableCell
+                    key={i}
+                    sx={{ ...tableStyles.tr.cell, p: 0.3, fontSize: "0.6rem" }}
+                    align="left"
+                  >
+                    {JSON.stringify(row[cell])?.replaceAll('"', "")}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
